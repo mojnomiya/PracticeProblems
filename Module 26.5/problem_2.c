323d4a2c0b6c5e84fa81e8820ee9b096f8f18fde
mojnomiya <mearn4706@gmail.com>
2022-12-01 17:12:33 +0600

    swap function updated

--- Module 26.5/problem_2.c
+++ Module 26.5/problem_2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 void sort(int *arr, int n);
-
+void swap(int *a, int *b);
 int main() {
     int n,i;
     scanf("%d", &n);
@@ -11,6 +11,7 @@ int main() {
     }
     int *ptr = &arr[0];
     for(i=0; i<n; i++)
+    
     sort(ptr, n);
     
     for(i=0; i<n; i++)
@@ -29,10 +30,54 @@ void sort(int *arr, int n)
         for(int j=i+1; j<n; j++)
         {
             if(arr[i]>arr[j]){
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int *a = &arr[i], *b = &arr[j];
+                swap(a,b);
             }
         }
     }
 }
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// #include <stdio.h>
+// void sort(int *arr, int n);
+
+// int main() {
+//     int n,i;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for(i=0; i<n; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     int *ptr = &arr[0];
+//     for(i=0; i<n; i++)
+//     sort(ptr, n);
+    
+//     for(i=0; i<n; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+    
+//     return 0;
+// }
+
+// void sort(int *arr, int n)
+// {
+//     int temp;
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int j=i+1; j<n; j++)
+//         {
+//             if(arr[i]>arr[j]){
+//                 temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+// }
